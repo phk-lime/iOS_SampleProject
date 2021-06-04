7a4c65acefb28d513fdeed83a17be2a18a821f8f
phk-lime <phk@limefriends.com>
2021-06-04 18:01:50 +0900

    컬렉션뷰 셀 동적 높이 구현 샘플

--- CollectionViewDynamicHeight/CollectionViewDynamicHeight/ViewController.swift
+++ CollectionViewDynamicHeight/CollectionViewDynamicHeight/ViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
+// MARK: - cell
+class MyCell: UICollectionViewCell {
+    
+}
