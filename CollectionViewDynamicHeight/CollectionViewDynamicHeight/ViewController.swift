99cf8871b7fce472d1ce751a8c6b054f4a629af2
phk-lime <phk@limefriends.com>
2021-06-05 20:33:29 +0900

    dynamic height 완료

--- CollectionViewDynamicHeight/CollectionViewDynamicHeight/ViewController.swift
+++ CollectionViewDynamicHeight/CollectionViewDynamicHeight/ViewController.swift
@@ -10,12 +10,51 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
+    var photos = ["1", "3", "4", "2"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let customLayout = DynamicHeightLayout()
+        customLayout.delegate = self
+        collectionView.collectionViewLayout = customLayout
+        collectionView.dataSource = self
     }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath) as? MyCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.layer.backgroundColor = UIColor.lightGray.cgColor
+        cell.imageView.image = UIImage(named: self.photos[indexPath.item])
+        cell.layer.cornerRadius = 12
+        cell.layer.masksToBounds = true
+        return cell
+    }
+}
+
+extension ViewController: DynaminHeightLayoutDelegate {
+    func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
+        let photo: UIImage?
+        let photoNumber = self.photos[indexPath.item]
+        photo = UIImage(named: photoNumber)
+        return photo?.size.height ?? 0
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
 }
 
 // MARK: - cell
 class MyCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
     
 }
